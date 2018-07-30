2fb8bbca452e0ced7b270e25d754909b9b889afe
tyknot <tyknott94@gmail.com>
2018-07-30 17:21:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,25 @@
-//Scroll to sections
-document.getElementById("about").addEventListener("click", (e) =>{
-	document.querySelector(".about").scrollIntoView({
-		behavior: "smooth",
-		block: "start",
-		inline: "nearest"
-	});
+window.addEventListener('scroll', (e) =>{
+
+	const el = document.querySelector(".header-scroll");
+	
+	// vertical scroll position
+	if(this.scrollY >= 100 && this.scrollY <= 300){
+		el.style.width = "15%";
+	}else if(this.scrollY >= 300 && this.scrollY <= 500){
+		el.style.width = "35%";
+	}else if(this.scrollY >= 500 && this.scrollY <= 1000){
+		el.style.width = "45%";
+	}else if(this.scrollY >= 1000 && this.scrollY <= 1300){
+		el.style.width = "65%";
+	}else if(this.scrollY >= 1300 && this.scrollY <= 1650){
+		el.style.width = "75%";
+	}else if(this.scrollY >= 1650 && this.scrollY <= 1800){
+		el.style.width = "85%";
+	}else if(this.scrollY >= 1800 && this.scrollY <= 1980){
+		el.style.width = "95%";
+	}else if(this.scrollY >= 1985){
+		el.style.width = "100%";
+	}else{
+		el.style.width = "0%";
+	}
 });
-
-document.getElementById("projects").addEventListener("click", (e) =>{
-	document.querySelector(".projects").scrollIntoView({
-		behavior: "smooth",
-		block: "start",
-		inline: "nearest"
-	});
-});
-
-document.getElementById("skills").addEventListener("click", (e) =>{
-	document.querySelector(".skills").scrollIntoView({
-		behavior: "smooth",
-		block: "start",
-		inline: "nearest"
-	});
-});
-
-document.getElementById("contact").addEventListener("click", (e) =>{
-	document.querySelector(".contact").scrollIntoView({
-		behavior: "smooth",
-		block: "end",
-		inline: "nearest"
-	});
-});
-
-
-
