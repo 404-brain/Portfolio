63153e7cfb535ae7a83ed7e06ef3988e7b7d9589
tyknot <tyknott94@gmail.com>
2018-12-07 17:47:37 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,51 @@
+const slider = document.querySelector('.slider');
+const contact = document.querySelector('#contact');
+const projects = document.querySelector('#projects');
+const skills = document.querySelector('#skills');
+const about = document.querySelector('#about');
+
 //Scroll to sections
-document.getElementById("about").addEventListener("click", (e) =>{
+document.getElementById("about").addEventListener("click", () =>{
 	document.querySelector(".about").scrollIntoView({
 		behavior: "instant",
 		block: "start",
 		inline: "nearest"
 	});
-});
+	if(true){
+		contact.style.color = 'white';
+		projects.style.color = 'white';
+		skills.style.color = 'white';
+		about.style.color = 'gray';
+	}
+}, false);
 
-document.getElementById("projects").addEventListener("click", (e) =>{
+document.getElementById("projects").addEventListener("click", () =>{
 	document.querySelector(".projects").scrollIntoView({
 		behavior: "instant",
 		block: "start",
 		inline: "nearest"
 	});
-});
+	if(true){
+		projects.style.color = 'gray';
+		contact.style.color = 'white';
+		skills.style.color = 'white';
+		about.style.color = 'white';
+	}
+}, false);
 
-document.getElementById("skills").addEventListener("click", (e) =>{
+document.getElementById("skills").addEventListener("click", () =>{
 	document.querySelector(".skills").scrollIntoView({
 		behavior: "instant",
 		block: "start",
 		inline: "nearest"
 	});
-});
+	if(true){
+		contact.style.color = 'white';
+		projects.style.color = 'white';
+		skills.style.color = 'gray';
+		about.style.color = 'white';
+	}
+}, false);
 
 document.getElementById("contact").addEventListener("click", (e) =>{
 	document.querySelector(".contact").scrollIntoView({
@@ -29,7 +53,10 @@ document.getElementById("contact").addEventListener("click", (e) =>{
 		block: "end",
 		inline: "end"
 	});
-});
-
-
-
+	if(true){
+		contact.style.color = 'gray';
+		projects.style.color = 'white';
+		skills.style.color = 'white';
+		about.style.color = 'white';
+	}
+}, false);
